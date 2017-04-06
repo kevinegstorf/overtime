6e0e7f0aca29351887ab5ce7e4507c1a84037be3
kevinegstorf <kevin.egstorf@gmail.com>
2017-04-06 15:44:03 +0200

    added Enum and status to workflow

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   include RailsAdmin::Post
+  enum status: { submitted: 0, approved: 1, rejected: 2 }
   belongs_to :user
   validates_presence_of :date, :rationale
 end
